7b1c18ed8d65151b753bc674d2f74f385dfc9a18
Prasath Ramasamy <prasath0906@gmail.com>
2022-12-07 23:28:13 -0500

    Updated to react

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rprasath'
+      className='link footer__link'
+    >
+      Prasath Ramasamy
+    </a>
+  </footer>
+)
+
+export default Footer;
---
+++ src/portfolio.js
@@ -0,0 +1,53 @@
+const header = {
+  homepage: 'https://prasathramasamy.com',
+  title: 'Prasath Ramasamy',
+}
+
+const about = {
+  name: 'Prasath Ramasamy',
+  role: 'Full Stack Engineer',
+  description:
+    'I am a Full Stack Engineer with 12+ years of experience in building web applications. I have worked on a wide range of technologies and projects in the past. I am passionate about building scalable and performant web applications.',
+  // resume: 'https://prasathramasamy.com/resume.pdf',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/prasathramasamy/',
+    github: 'https://github.com/rprasath',
+  },
+}
+
+const projects = [
+  // {
+  //   name: 'Project 1',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // }
+]
+
+const skills = [
+  'C#',
+  '.NET',
+  'ASP.NET',
+  'ASP.NET Core',
+  'Azure DevOps',
+  'Azure Administration',
+  'Azure Development',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Angular',
+  'Material UI',
+  'Git',
+  'CI/CD',
+  'Browser Extensions'
+]
+
+const contact = {
+  email: 'contact@prasath.me',
+}
+
+export { header, about, projects, skills, contact }
